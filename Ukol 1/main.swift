ada292499d3777875f6a729a3e40537f6acefa9a
Matěj Žemlička <43809215+Zemlaman@users.noreply.github.com>
2019-09-26 22:26:09 +0200

    Add files via upload

---
+++ Ukol 1/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Ukol 1
+//
+//  Created by Matěj Žemlička on 26/09/2019.
+//  Copyright © 2019 Matěj Žemlička. All rights reserved.
+//
+
+import Foundation
+
+var a = "yxyxyxyx";
+var b = "xyxyxyxy";
+
+for i in 1...8 {
+    if i % 2 == 0 {
+        print(a);
+    } else {
+        print(b);
+    }
+}
+
